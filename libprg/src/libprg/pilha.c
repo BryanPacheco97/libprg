4213b22ae687aa7416b29ee9d948f89ee0963463
bryan <bryan.lp1997@aluno.ifsc.edu.br>
2025-09-04 08:00:16 -0300

    Biblioteca quase concluida.

--- libprg/src/libprg/pilha.c
+++ libprg/src/libprg/pilha.c
@@ -9,57 +9,81 @@ typedef struct pilha{
 } pilha_t;
 
 
-    pilha_t* criar_pilha(int capacidade) {
-        pilha_t* p = malloc(sizeof(pilha_t));
+pilha_t* criar_pilha(int capacidade) {
+    pilha_t* p = malloc(sizeof(pilha_t));
 
-        p->elementos = malloc(capacidade * sizeof(int));
+    p->elementos = malloc(capacidade * sizeof(int));
 
-        p->topo = -1;
+    p->topo = -1;
 
-        p->capacidade = capacidade;
+    p->capacidade = capacidade;
 
-        return p;
+    return p;
+
+}
+
+int empilhar_a_pilha(pilha_t* p, int valor) {
+    if (p->topo == p->capacidade - 1) {  // Se o conteudo do valor de topo for igual a capacidade -1
+        printf("\n A Pilha esta cheia!!!!\n"); //
+        return 0;
+    }
+    p->elementos[++p->topo] = valor; /* ocorre primeiro um incremento de topo,
+                                        depois salva o elemento do vetor na variavel valor */
+    return 1;
+
+}
+int desempilhar_a_pilha(pilha_t* p) {
+    if (p-> topo == -1) {  // Se o valor do campo da estruct eh igual a -1
+        printf("\n A pilha esta vazia!!!! \n");
+
+    }
+    return p->elementos[p->topo--]; // decrementa o elemento do vetor
+
+
+}
+
+int tamanho_pilha(pilha_t* p) {
+    return p->topo +1;
+}
+
+
+int imprime_topo_pilha(pilha_t* p) {
+    if (p->topo == -1) {
+        printf("\n A pilha esta vazia!!!! \n");
+    }
+
+    return p->elementos[p->topo];
+
+
+
+}
+
+void imprime_pilha_atual(pilha_t* p) {
+    if (p->topo == -1) {
+        printf("\n Pilha está vazia!!!\n");
 
     }
 
-  int empilhar_a_pilha(pilha_t* p, int valor) {
-        if (p->topo == p->capacidade - 1) {  // Se o conteudo do valor de topo for igual a capacidade -1
-            printf("\n A Pilha esta cheia!!!!\n"); //
-            return 0;
-        }
-        p->elementos[++p->topo] = valor; /* ocorre primeiro um incremento de topo,
-                                            depois salva o elemento do vetor na variavel valor */
-            return 1;
-
-    }
-    int desempilhar_a_pilha(pilha_t* p) {
-        if (p-> topo == -1) {  // Se o valor do campo da estruct eh igual a -1
-            printf("\n A pilha esta vazia!!!! \n");
-
-        }
-        return p->elementos[p->topo--]; // decrementa o elemento do vetor
-
-
-    }
-
-    void tamanho_pilha(pilha_t* p) {
-        if (p-> topo == -1) {
-            printf("\n A pilha esta vazia!!!! \n");
-        } else {
-            printf("\n tamanho da pilha eh %d", p->topo +1);
-        }
-
+    printf("Os elementos restantes na pilha:\n");
+    int i;
+    for (i=0; i <= p->topo; i++) {
+        printf("%d", p->elementos[i]);
 
     }
 
 
 
-    void destruir_a_pilha(pilha_t* p) {   // nao preciso de retorno, por isso um void
-        free(p->elementos); // liberei a memoria alocada do vetor elementos
-        free(p);               // liberei a memoria alocada do ponteiro p
+}
 
 
-    }
+
+
+void destruir_a_pilha(pilha_t* p) {   // nao preciso de retorno, por isso um void
+    free(p->elementos); // liberei a memoria alocada do vetor elementos
+    free(p);               // liberei a memoria alocada do ponteiro p
+
+
+}
 
 
 
@@ -68,10 +92,10 @@ typedef struct pilha{
 
 
 /*
-    desempilhar
-    tamanho
+    desempilhar ok
+    tamanho ok
     vazia
-    destruir
+    destruir ok
     */
 
 
